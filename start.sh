79ea2869bc049941cbc29303477a18c1bcf5512b
Ismael Bencharef <66883069+IshShogun@users.noreply.github.com>
2024-04-29 11:18:07 +0400

    commit

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update pip
+python -m pip install --upgrade pip
+
+# Install scikit-learn (sklearn)
+pip install scikit-learn
+
+# Install SciPy
+pip install scipy
+
+# Install NumPy
+pip install numpy
+
+echo "Installation complete!"
